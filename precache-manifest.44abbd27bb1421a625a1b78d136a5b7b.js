bf4fab49131aaa481fa95ac73039f20623bd7e19
5377 <pradipamate07@gmail.com>
2020-07-07 15:43:03 +0530

    Updates

---
+++ precache-manifest.44abbd27bb1421a625a1b78d136a5b7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "be80af73b37119c55a97",
+    "url": "/Puzzle/static/css/main.e9ee6f43.chunk.css"
+  },
+  {
+    "revision": "be80af73b37119c55a97",
+    "url": "/Puzzle/static/js/main.9ee20cac.chunk.js"
+  },
+  {
+    "revision": "e89c5b19bffa0d939602",
+    "url": "/Puzzle/static/js/runtime~main.ddaccd2c.js"
+  },
+  {
+    "revision": "5201912d341de0c76a94",
+    "url": "/Puzzle/static/js/2.783a707b.chunk.js"
+  },
+  {
+    "revision": "81d152a7ba325c0927fc19d84755dd50",
+    "url": "/Puzzle/index.html"
+  }
+];
